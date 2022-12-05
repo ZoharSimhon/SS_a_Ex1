14937c3bf5f6c067d7d30b505345a7d7924c87c6
ZoharSimhon <118903543+ZoharSimhon@users.noreply.github.com>
2022-12-05 23:02:53 +0200

    Delete my_mat.h

--- my_mat.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_MAT_H
-#define MY_MAT_H
-#define size_mat 10
-void get_matrix_values();
-int is_there_path (int, int);
-int shortest_path (int, int);
-void floyd_warshall_algorithm ();
-int min (int, int);
-#endif
